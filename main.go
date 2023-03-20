4d71a64a07ee5ab88df783cb70fbfb4d5d95b808
Weijia <caivega@163.com>
2023-03-20 22:13:15 +0800

    update the main

--- main.go
+++ main.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"os"
-
 	"github.com/tokentransfer/node/conf"
 	"github.com/tokentransfer/node/net"
-	"github.com/tokentransfer/node/vm"
 )
 
 func main() {
@@ -34,19 +29,4 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
-	f, err := os.Open("./lib.wasm")
-	if err != nil {
-		panic(err)
-	}
-	wasmBytes, err := io.ReadAll(f)
-	if err != nil {
-		panic(err)
-	}
-
-	results, err := vm.RunWasm(wasmBytes, "sum", 4.2, 3.1)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(results)
 }
