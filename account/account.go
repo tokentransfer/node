e255b0bb38b0cc0eff361775a74d13a601c81942
Weijia <caivega@163.com>
2023-03-22 10:36:02 +0800

    add certificate.sh

--- account/account.go
+++ account/account.go
@@ -1,6 +1,7 @@
 package account
 
 import (
+	"github.com/tokentransfer/node/account/eth"
 	"github.com/tokentransfer/node/account/jingtum"
 
 	libaccount "github.com/tokentransfer/interfaces/account"
@@ -8,16 +9,16 @@ import (
 )
 
 const (
-	// ETH     libaccount.KeyType = 0
+	ETH     libaccount.KeyType = 0
 	JINGTUM libaccount.KeyType = 1
 )
 
 func init() {
 	// KeyType
-	// ETH.Register("ETH", func(t libaccount.KeyType) libaccount.Key {
-	// 	key, _ := eth.GenerateFamilySeed("masterpassphrase")
-	// 	return key
-	// })
+	ETH.Register("ETH", func(t libaccount.KeyType) libaccount.Key {
+		key, _ := eth.GenerateFamilySeed("masterpassphrase")
+		return key
+	})
 	JINGTUM.Register("Jingtum", func(t libaccount.KeyType) libaccount.Key {
 		key, _ := jingtum.GenerateFamilySeed("masterpassphrase")
 		return key
@@ -28,63 +29,63 @@ type AccountService struct {
 }
 
 func (service *AccountService) GenerateFamilySeed(password string) (libaccount.KeyType, libaccount.Key, error) {
-	key, err := jingtum.GenerateFamilySeed(password)
+	key, err := eth.GenerateFamilySeed(password)
 	if err != nil {
 		return 0, nil, err
 	}
-	return JINGTUM, key, nil
+	return ETH, key, nil
 }
 
 func (service *AccountService) NewKeyFromSecret(secret string) (libaccount.KeyType, libaccount.Key, error) {
-	key := &jingtum.Key{}
+	key := &eth.Key{}
 	err := key.UnmarshalText([]byte(secret))
 	if err != nil {
 		return 0, nil, err
 	}
-	return JINGTUM, key, nil
+	return ETH, key, nil
 }
 
 func (service *AccountService) NewKeyFromBytes(data []byte) (libaccount.KeyType, libaccount.Key, error) {
-	key := &jingtum.Key{}
+	key := &eth.Key{}
 	err := key.UnmarshalBinary(data)
 	if err != nil {
 		return 0, nil, err
 	}
-	return JINGTUM, key, nil
+	return ETH, key, nil
 }
 
 func (service *AccountService) NewPublicFromHex(s string) (libaccount.KeyType, libaccount.PublicKey, error) {
-	key := &jingtum.Public{}
+	key := &eth.Public{}
 	err := key.UnmarshalText([]byte(s))
 	if err != nil {
 		return 0, nil, err
 	}
-	return JINGTUM, key, nil
+	return ETH, key, nil
 }
 
 func (service *AccountService) NewPublicFromBytes(data []byte) (libaccount.KeyType, libaccount.PublicKey, error) {
-	key := &jingtum.Public{}
+	key := &eth.Public{}
 	err := key.UnmarshalBinary(data)
 	if err != nil {
 		return 0, nil, err
 	}
-	return JINGTUM, key, nil
+	return ETH, key, nil
 }
 
 func (service *AccountService) NewAccountFromAddress(address string) (libaccount.KeyType, libcore.Address, error) {
-	a := &jingtum.Address{}
+	a := &eth.Address{}
 	err := a.UnmarshalText([]byte(address))
 	if err != nil {
 		return 0, nil, err
 	}
-	return JINGTUM, a, nil
+	return ETH, a, nil
 }
 
 func (service *AccountService) NewAccountFromBytes(data []byte) (libaccount.KeyType, libcore.Address, error) {
-	a := &jingtum.Address{}
+	a := &eth.Address{}
 	err := a.UnmarshalBinary(data)
 	if err != nil {
 		return 0, nil, err
 	}
-	return JINGTUM, a, nil
+	return ETH, a, nil
 }
