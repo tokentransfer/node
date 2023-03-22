688fb741f9a02131ee7e8b70f1a68e7d39366311
Weijia <caivega@163.com>
2023-03-22 11:06:10 +0800

    move node.* to test dir

--- test/certificate.sh
+++ test/certificate.sh
@@ -1,12 +1,12 @@
-echo '生成node.key'
+echo '> 生成node.key'
 openssl genrsa -des3 -out node.key 2048
-echo '生成不需要密码的node.key'
+echo '> 生成不需要密码的node.key'
 openssl rsa -in node.key -out node.key
-echo '生成ca的crt'
+echo '> 生成ca的crt'
 openssl req -new -x509 -key node.key -out ca.crt -days 365
-echo '生成csr'
+echo '> 生成csr'
 openssl req -new -key node.key -out node.csr
-echo '生成crt'
+echo '> 生成crt'
 openssl x509 -req -days 365 -in node.csr -CA ca.crt -CAkey node.key -CAcreateserial -out node.crt
-echo '证书合并'
+echo '> 证书合并'
 cat node.key node.crt > node.pem
