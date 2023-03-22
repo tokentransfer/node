11f68f6c2c51229366331cac08b1d2d1f01cfcc5
Weijia <caivega@163.com>
2023-03-22 11:21:24 +0800

    update certificate.sh

--- test/certificate.sh
+++ test/certificate.sh
@@ -1,9 +1,10 @@
+# echo '> 生成ca的crt'
+# openssl req -new -x509 -key server.key -out ca.crt -days 365
+
 echo '> 生成node.key'
 openssl genrsa -des3 -out node.key 2048
 echo '> 生成不需要密码的node.key'
 openssl rsa -in node.key -out node.key
-echo '> 生成ca的crt'
-openssl req -new -x509 -key node.key -out ca.crt -days 365
 echo '> 生成csr'
 openssl req -new -key node.key -out node.csr
 echo '> 生成crt'
