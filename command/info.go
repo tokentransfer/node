d7831e3f55732fefdd2f5d044a14c8820cd14aad
Weijia <caivega@163.com>
2023-05-11 16:19:19 +0800

    update info commmand

--- command/info.go
+++ command/info.go
@@ -1,13 +1,16 @@
 package command
 
 import (
-	"bytes"
 	"flag"
-	"fmt"
-	"sort"
 	"strings"
 
 	"github.com/mitchellh/cli"
+
+	"github.com/tokentransfer/node/account"
+	"github.com/tokentransfer/node/config"
+	"github.com/tokentransfer/node/util"
+
+	libcore "github.com/tokentransfer/interfaces/core"
 )
 
 // InfoCommand is a Command implementation that queries a running
@@ -23,59 +26,62 @@ func (i *InfoCommand) Help() string {
 Usage: node info [options]
 	Provides debugging information for operators
 Options:
-  -format                  If provided, output is returned in the specified
-                           format. Valid formats are 'json', and 'text' (default)
-  -rpc-auth=""             RPC auth token of the Node agent.
+	-config		Path to a JSON file to read configuration from.
+	-account		The account
 `
 	return strings.TrimSpace(helpText)
 }
 
 func (i *InfoCommand) Run(args []string) int {
-	var format string
+	var configFile string
+	var accountString string
+
 	cmdFlags := flag.NewFlagSet("info", flag.ContinueOnError)
 	cmdFlags.Usage = func() { i.Ui.Output(i.Help()) }
-	cmdFlags.StringVar(&format, "format", "text", "output format")
+	cmdFlags.StringVar(&configFile, "config", "./config.json", "json file to read config from")
+	cmdFlags.StringVar(&accountString, "account", "", "the account")
 	if err := cmdFlags.Parse(args); err != nil {
 		return 1
 	}
 
-	i.Ui.Output(string(""))
+	config, err := config.NewConfig(configFile)
+	if err != nil {
+		panic(err)
+	}
+	as := account.NewAccountService()
+
+	if len(config.GetSecret()) == 0 {
+		secret, err := i.Ui.AskSecret("The secret:")
+		if err != nil {
+			panic(err)
+		}
+		config.SetSecret(secret)
+	}
+
+	_, nodeKey, err := as.NewKeyFromSecret(config.GetSecret())
+	if err != nil {
+		panic(err)
+	}
+	nodeAccount, err := nodeKey.GetAddress()
+	if err != nil {
+		panic(err)
+	}
+	i.Ui.Info("Node: " + util.GetString(nodeAccount))
+
+	var account libcore.Address
+	if len(accountString) > 0 {
+		_, a, err := as.NewAccountFromAddress(accountString)
+		if err != nil {
+			panic(err)
+		}
+		account = a
+	} else {
+		account = nil
+	}
+	i.Ui.Info("Account: " + util.GetString(account))
 	return 0
 }
 
 func (i *InfoCommand) Synopsis() string {
 	return "Provides debugging information for operators"
-}
-
-type StatsContainer map[string]map[string]string
-
-func (s StatsContainer) String() string {
-	var buf bytes.Buffer
-
-	// Get the keys in sorted order
-	keys := make([]string, 0, len(s))
-	for key := range s {
-		keys = append(keys, key)
-	}
-	sort.Strings(keys)
-
-	// Iterate over each top-level key
-	for _, key := range keys {
-		buf.WriteString(fmt.Sprintf(key + ":\n"))
-
-		// Sort the sub-keys
-		subvals := s[key]
-		subkeys := make([]string, 0, len(subvals))
-		for k := range subvals {
-			subkeys = append(subkeys, k)
-		}
-		sort.Strings(subkeys)
-
-		// Iterate over the subkeys
-		for _, subkey := range subkeys {
-			val := subvals[subkey]
-			buf.WriteString(fmt.Sprintf("\t%s = %s\n", subkey, val))
-		}
-	}
-	return buf.String()
 }
